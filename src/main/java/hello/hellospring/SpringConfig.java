20b1eb5fca88dac33b4b86ba195d2a1837d8d7c4
DESKTOP-HO84V0D\hoya <icepega@gmail.com>
2022-02-06 19:56:46 +0900

    jpa version commit

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -6,31 +6,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
-    private final MybatisMapper mybatisMapper;
+//    private final DataSource dataSource;
+//    private final MybatisMapper mybatisMapper;
+//    private final EntityManager em;
 
-    @Autowired
-    public SpringConfig(DataSource dataSource , MybatisMapper mybatisMapper) {
+    private final MemberRepository memberRepository;
 
-        this.dataSource = dataSource;
-        this.mybatisMapper = mybatisMapper;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService(){
 
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-//        return new MemoryMemberRepository();
-        return new JdbcTemplateMemberRepository(dataSource);
-//        return new MybatisRepository(mybatisMapper);
-    }
+//    @Bean
+//    public MemberRepository memberRepository(){
+////        return new MemoryMemberRepository();
+////        return new JdbcTemplateMemberRepository(dataSource);
+////        return new MybatisRepository(mybatisMapper);
+//        return new JpaMemberRepository(em);
+//    }
 }
